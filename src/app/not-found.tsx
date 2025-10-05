11d64d61df6f89292066f6243499a147e1bf1285
SPACECHILD <spacechild7@gmail.com>
2025-10-05 22:35:32 +0900

    feat : not-found.tsx
    - gradient-to-r
    - to-transparent

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -14,20 +14,20 @@ const publicSans = Public_Sans({
 
 export default function NotFound() {
   return (
-    <main className={`${publicSans.className}`}>
-      <section className="relative flex h-screen items-center bg-black px-8 py-18">
-        <div className="w-full mb-12">
-          <div className="w-full max-w-md z-40">
+    <main className={`${publicSans.className} min-w-xs`}>
+      <section className="relative flex h-screen items-center bg-black px-[32px] py-[72px]">
+        <div className="relative w-full px-10 mb-12 z-20">
+          <div className="container max-w-[471px]">
             <h1
-              className={`${dmMono.className} text-[80px] leading-none font-medium text-white my-[53.6px]`}
+              className={`${dmMono.className} text-[48px] sm:text-[48px] md:text-[72px] lg:text-[120px] leading-none font-normal text-white my-20`}
             >
               404
             </h1>
-            <p className="text-lg font-bold text-white leading-relaxed my-[18px]">
-              The cosmic object you were looking for has disappeard beyond the
+            <p className="text-[18px] sm:text-[18px] md:text-[22px] text-white font-semibold leading-relaxed my-4 max-w-[600px] md:max-w-[720px] lg:max-w-[900px]">
+              The cosmic object you were looking for has disappeared beyond the
               event horizon.
             </p>
-            <div className="flex flex-wrap items-center gap-4 mt-6 font-bold">
+            <div className="flex flex-wrap items-center gap-9 mt-6 font-bold ">
               <Link
                 href="/"
                 className="inline-block text-white border-b border-dashed pb-2"
@@ -53,6 +53,17 @@ export default function NotFound() {
             </div>
           </div>
         </div>
+        <div
+          className="absolute inset-0 bg-gradient-to-r from-black/80 to-transparent z-10"
+          aria-hidden="true"
+        ></div>
+        <Image
+          src="/image/404-bg.jpg"
+          alt="404"
+          fill
+          priority
+          className="absolute inset-0 object-cover z-0"
+        ></Image>
       </section>
     </main>
   );
